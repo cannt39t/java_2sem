650495f89f8ff5d5360f8108e5cd1f0764663738
Ilya Kaznacheev <ilyakajava@gmail.com>
2022-09-15 23:07:22 +0300

    02: Servelt and HttpClient

---
+++ 01Lesson/src/main/java/com/kaznacheev/net/server/HelloServlet.java
@@ -0,0 +1,44 @@
+package com.kaznacheev.net.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Collectors;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello!");
+    }
+
+}
+
+@WebServlet(name = "post", urlPatterns = "/post")
+class Post extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String uri = req.getRequestURI();
+        String params = formatParams(req);
+        resp.getWriter().write("Method doPost\nURI: " + uri + "\nParams:\n" + params + "\n");
+    }
+
+
+    private String formatParams(HttpServletRequest req) {
+        return req.getParameterMap()
+                .entrySet()
+                .stream()
+                .map(entry -> {
+                    String param = String.join(" and ", entry.getValue());
+                    return entry.getKey() + " => " + param;
+                })
+                .collect(Collectors.joining("\n"));
+    }
+}
