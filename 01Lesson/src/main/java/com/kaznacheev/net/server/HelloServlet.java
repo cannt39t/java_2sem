af2d128d1fc30d414ed2ad8336eb75682f7cf590
Ilya Kaznacheev <ilyakajava@gmail.com>
2022-09-25 13:42:23 +0300

    02: Added removed files

--- 01Lesson/src/main/java/com/kaznacheev/net/server/HelloServlet.java
+++ 01Lesson/src/main/java/com/kaznacheev/net/server/HelloServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.stream.Collectors;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
@@ -15,5 +16,29 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         writer.println("Hello!");
+    }
+
+}
+
+@WebServlet(name = "post", urlPatterns = "/post")
+class Post extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String uri = req.getRequestURI();
+        String params = formatParams(req);
+        resp.getWriter().write("Method doPost\nURI: " + uri + "\nParams:\n" + params + "\n");
+    }
+
+
+    private String formatParams(HttpServletRequest req) {
+        return req.getParameterMap()
+                .entrySet()
+                .stream()
+                .map(entry -> {
+                    String param = String.join(" and ", entry.getValue());
+                    return entry.getKey() + " => " + param;
+                })
+                .collect(Collectors.joining("\n"));
     }
 }
